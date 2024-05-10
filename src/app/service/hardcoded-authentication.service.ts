fe3387be605978afebafc0a44abd5844ee6008f7
Kamesh Kumar <kameshkumar4602@gmail.com>
2024-05-10 18:57:50 +0530

    Session Storage

--- src/app/service/hardcoded-authentication.service.ts
+++ src/app/service/hardcoded-authentication.service.ts
@@ -8,9 +8,26 @@ export class HardcodedAuthenticationService {
   constructor() { }
 
   authenticate(username: string,password: string){
+    console.log('Before '+ this.isUserLoggedIn());
     if(username==="kamesh" && password==="dummy"){
+      sessionStorage.setItem('authenticaterUser',username) ;   
+      console.log('After '+ this.isUserLoggedIn());
       return true;
+
     }
     return false;
   }
+
+  isUserLoggedIn(){
+      let user = sessionStorage.getItem('authenticaterUser');
+      if (user !== null) {
+          return true;  // User is logged in
+      } else {
+          return false; // User is not logged in
+      }
+  
+  }
+
+
+
 }
