e7394a7968b316c5c868abde5b4ea98fef2a3cea
Kamesh Kumar <kameshkumar4602@gmail.com>
2024-05-11 21:05:44 +0530

    observable and connecting to backend

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -12,10 +13,17 @@ export class WelcomeComponent implements OnInit {
 
   name=''
 
-  constructor(private route:ActivatedRoute) { }
+  constructor(private route:ActivatedRoute,
+    private service:WelcomeDataService) { }
 
   ngOnInit(): void {
     this.name=this.route.snapshot.params['name']
+  }
+
+  getWelcomeMessage(){
+    console.log(this.service.executeHelloWorldBeanService());
+    this.service.executeHelloWorldBeanService().subscribe();
+  
   }
 
 
