7babe19ccefc464834701301f11dc2069bf5940e
Kamesh Kumar <kameshkumar4602@gmail.com>
2024-05-14 00:57:22 +0530

    TodoService

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoDataService } from '../service/data/todo-data.service';
 
 export class Todo{
   constructor(
@@ -18,23 +19,30 @@ export class Todo{
 })
 export class ListTodosComponent implements OnInit {
 
-  todos=[
-    new Todo(1,'abc',false,new Date()),
-    new Todo(2,'def',false,new Date()),
-    new Todo(3,'ghi',false,new Date()),
-    // {id:1,description:'abc'},
-    // {id:2,description:'def'},
-    // {id:3,description:'ghi'}
-  ]
+  todos:Todo[] | undefined
+  // =[
+  //   new Todo(1,'abc',false,new Date()),
+  //   new Todo(2,'def',false,new Date()),
+  //   new Todo(3,'ghi',false,new Date()),
+  //   // {id:1,description:'abc'},
+  //   // {id:2,description:'def'},
+  //   // {id:3,description:'ghi'}
+  // ]
 
   // todo={
   //   id:1,
   //   description:'Learn to Dance'
   // }
 
-  constructor() { }
+  constructor(private todoService:TodoDataService ) { }
 
   ngOnInit(): void {
+    this.todoService.retrieveAllTodos('username').subscribe(
+      response=>{
+        console.log(response)
+        this.todos=response;
+      }
+    )
   }
 
 }
--- src/app/service/data/todo-data.service.ts
+++ src/app/service/data/todo-data.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Todo } from 'src/app/list-todos/list-todos.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoDataService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  retrieveAllTodos(username: any){
+    return this.http.get<Todo[]>(`http://localhost:8080/users/${username}/todos`)
+    // console.log("uhdubssu")
+  }
 }
